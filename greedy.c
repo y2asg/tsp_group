aa8667580761536667330cef9840d699977b024f
Haruka Matsumoto <harukamatsumoto@Harukas-MacBook-Air.local>
2015-06-27 11:48:58 +0900

    write solution to a file

--- greedy.c
+++ greedy.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
 
 typedef struct point_type_t{
   double x;
@@ -9,65 +11,85 @@ typedef struct point_type_t{
 
 #define LENGTH 5
 point_type table[LENGTH];
+int solution[LENGTH];
 
-double distance(double x1,double x2,double y1, double y2){
-  return sqrt(pow(x1-x2,2) +  pow(y1-y2,2));
+
+double distance(point_type a,point_type b){
+  return sqrt(pow(a.x-b.x,2) +  pow(a.y-b.y,2));
 }
 
 void read_file(char* fname){
   FILE *fp;
   int i=0;
   double f1, f2;
-  char xname[10],yname[10];
+  char dammy[10];
 
   if((fp = fopen( fname, "r" ))==NULL){
     printf( "Can't open %s\n", fname );
+    exit(1);
+  }
+  fscanf( fp, "%[^,],%s",dammy,dammy);   //[^,] avoid reading ','  
+  for(int i=0; i<LENGTH; i++) {
+    table[i].passed=0;
+    if((fscanf( fp, "%lf,%lf",&table[i].x, &table[i].y) ) == EOF){ 
+      break;   //end of file
+    }
   }
 
-  
-    for(int i=0; i<LENGTH+1; i++) {
-        if(i==0){
-            fscanf( fp, "%[^,],%s",xname,yname);   //[^,] avoid reading ','
-                }
-        else{
-            table[i-1].passed=1;
-                if((fscanf( fp, "%lf,%lf",&table[i-1].x, &table[i-1].y) ) != EOF){
-                }
-        }
-       }
+  fclose(fp);
+}
+
+void output_file(void){
+  FILE *fp;
+  char *fname = "solution1.csv";
+  int i;
+
+  if((fp = fopen(fname, "w")) == NULL){
+    printf( "Can't open %s\n", fname );
+    exit(1);
+  }
+
+  for(i = 0; i < LENGTH; i++){
+    if(fprintf(fp, "%d\n",solution[i])<0){
+      break; //failed to write
+    }
+  }
   fclose(fp);
 }
 
 
 int main(void){
-  int i,index,pointer=0;
+  int i,index,pointer=0;//current pointer
   read_file("input_0.csv");
   
-  double min = 1000000000;
+  double min;
+  int passed_n =1;
 
-  int passed_n =0;//current pointer
-  table[pointer].passed =0;
+  table[pointer].passed =1;
+  solution[passed_n]=pointer;
   printf("%d\n", pointer);
 
-  while(passed_n < LENGTH-1){
+  while(passed_n < LENGTH){
     min = 1000000000;
+
     for(i=0;i<LENGTH;i++){
-      if(table[i].passed && i!=pointer){
-          double dist = distance(table[pointer].x,table[pointer].y,
-			    table[i].x,table[i].y);
+      if(!(table[i].passed) && i!=pointer){
+          double dist = distance(table[pointer],table[i]);
           //printf("%d %d %lf\n",pointer, i, dist);
           if( dist < min){
               min = dist;
               index = i;
           }
       }
-    }
-     
-    table[index].passed = 0;
+    } 
     pointer = index; //move pointer
+    table[index].passed = 1;
+    solution[passed_n]=index;
     printf("%d\n",index);
     passed_n ++;
   }
+
+  output_file();
   return 0;
 
 }
