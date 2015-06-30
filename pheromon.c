3393df2811a6c6059ef0291e454aa42883382aae
yuumin <yuko0904k@gmail.com>
2015-07-01 00:15:40 +0900

    分泌量Δを決める関数 delta_pheromon

--- pheromon.c
+++ pheromon.c
@@ -1,1 +1,13 @@
 /*分泌量Δを決める関数*/
+//引数として、そのアリｋちゃんが作った巡回経路の距離の総和ant.tour_lenをわたす。
+//分泌量はもしアリkちゃんが通った辺にはQ÷距離を返す。通ってなかったら０を返す。、
+void delta_pheromon(double ant.tour_len){
+  int Q=100; //定数
+  if(checkpath(int a , int b)==1){　//辺(a,b)が巡回経路の辺になっていたら
+    delta=Q/ant.tour_len;
+    return delta;
+  }else{
+    return 0;
+  }
+}
+/*t回目からt+1回目更新*/
