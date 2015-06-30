734cb88e4e03525106ce7acd91eb017d26ea40b1
yuumin <yuko0904k@gmail.com>
2015-07-01 01:18:58 +0900

    配列を引数に渡すやり方が未解決です

--- checkpath.c
+++ checkpath.c
@@ -3,7 +3,7 @@
 //引数は、地点aとb(a,b=頂点の名前（0 or 1 or 2 or …）)とそのときの巡回路の通った順番を表すant.solution[CITY_NUM]
 //ant.solutiton[0]=0 で、かつ、ant.solution[CITY_NUM]（最後もう一度０）=0です
 
-void checkpath(int a, int b, ant.solution[CITY_NUM]){
+void checkpath(int a, int b, int ant.solution[CITY_NUM]){ //配列を引数に渡すやり方が未解決です
 	int i=0;
 	for(i=0;i<=CITY_NUM;i++){
 		if(ant.solution[i]==a){
