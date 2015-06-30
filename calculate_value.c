0ba69588b6e042c10f5ef7735423621f7508895d
yuumin <yuko0904k@gmail.com>
2015-07-01 02:40:04 +0900

    つらみ

--- calculate_value.c
+++ calculate_value.c
@@ -1,1 +1,11 @@
-/*評価値　あるアリｋgが都市iにいるとき、まだ訪れていない都市jへ行くかどうかの評価値を計算する  a_(i,j,k)*/
+/*評価値　あるアリｋが都市iにいるとき、まだ訪れていない都市jへ行くかどうかの評価値を計算する  a_(i,j,k)*/
+
+void calucate_value(double now_per, double dist, int not_visited) { //not_visited=まだ訪れていない都市数
+
+
+void caluculate_total(   ){
+  double total;
+int i;
+  for(i=0;i< not_visited ;i++){
+    siguma+=pow(now_per)
+  }
