1f32719f80c7ab5fedeab518239a40ef67564f34
yuumin <yuko0904k@gmail.com>
2015-07-01 01:01:18 +0900

    フェロモン情報を更新する関数

--- pheromon.c
+++ pheromon.c
@@ -11,4 +11,15 @@ void delta_pheromon(double ant.tour_len){
     return 0;
   }
 }
-/*t回目からt+1回目更新*/
+/*ありｋちゃんが巡回経路を作り終えるごとにフェロモンを更新するupdate_pheromon関数。蒸発率ρはどう設定するかは検討の必要あり*/
+//何を引数とすればいいのかな
+
+void update_pher(int now_pher){ //now_pheromon=t回の時のフェロモン蓄積量　new=t+1回目の蓄積量
+  int new_pher=0;//next quantity of pheromon 次更新後のフェロモン蓄積量
+  int delta=0;
+  int RHO=0.8; //検討が必要
+  delta=delta_pher(double ant.tour_len);//追加分泌量
+  new_pher=RHO*now_pher+delta;//更新
+  return new_pher;
+}
+
