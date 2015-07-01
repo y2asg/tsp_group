a5e51aadf7a3cf7871a0d8725bb3839dcbd9cdbc
yuumin <yuko0904k@gmail.com>
2015-07-01 11:15:58 +0900

    引数を修正。都市ab間のフェロモン量を更新させる関数

--- pheromon.c
+++ pheromon.c
@@ -1,25 +1,45 @@
 /*分泌量Δを決める関数*/
 //引数として、そのアリｋちゃんが作った巡回経路の距離の総和ant.tour_lenをわたす。
 //分泌量はもしアリkちゃんが通った辺にはQ÷距離を返す。通ってなかったら０を返す。、
-void delta_pheromon(double ant.tour_len){
+void delta_pheromon(double ant.tour_len,city_a,city_b){
+  checkpath(city_a,city_b);
   int Q=100; /定数
   int delta=0;
-  if(checkpath(int a , int b)==1){　//辺(a,b)が巡回経路の辺になっていたら
+  if(checkpath(city_a,city_b)==1){　//辺(a,b)が巡回経路の辺になっていたら
     delta=Q/ant.tour_len;
     return delta;
   }else{
     return 0;
   }
 }
-/*ありｋちゃんが巡回経路を作り終えるごとにフェロモンを更新するupdate_pheromon関数。蒸発率ρはどう設定するかは検討の必要あり*/
-//何を引数とすればいいのかな
 
-void update_pher(int now_pher){ //now_pheromon=t回の時のフェロモン蓄積量　new=t+1回目の蓄積量
-  int new_pher=0;//next quantity of pheromon 次更新後のフェロモン蓄積量
-  int delta=0;
-  int RHO=0.8; //検討が必要
-  delta=delta_pher(double ant.tour_len);//追加分泌量
-  new_pher=RHO*now_pher+delta;//更新
-  return new_pher;
+/*ｔ回目からt+1回目のうちにフェロモンが蒸発するのを表す関数*/
+void decay_pheromon(int pheromon){
+    pheromon *= (1.0 - RHO);
+    return pheromon
 }
+
+
+/*ありｋちゃんが巡回経路を作り終えるごとにフェロモンを更新するupdate_pheromon関数。蒸発率ρはどう設定するかは検討の必要あり*/
+//a,b都市間の辺のフェロモンを更新させるように作ってある
+//引数はテーブルfiels[i][j]が扱いやすいように都市a,bを渡す
+void update_pher(int city_a, int city_b,double* fields , double ant.tour_len){
+  double field=0;
+  double pheromon=0;
+  double new_fields[][]
+  
+  field = fields[city_a][city_b];
+      new_pher=0://初期化
+      if(city_a!=city_b){
+                pheromon = decay_pheromon(field); //蒸発する
+                delta=delta_pheromon(ant.tour_len);//追加分泌量
+                new_pher=pherimin+delta;
+                return new_pher;
+      }else{
+       0;  
+      }
+}
+//メイン関数内でfieldsの更新を書く
+
+
 
