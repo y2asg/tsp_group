240392f11c4ad1b08a81183df98ca66f7c696f70
yuumin <yuko0904k@gmail.com>
2015-07-01 11:26:28 +0900

    ポインタを引数にした

--- checkpath.c
+++ checkpath.c
@@ -3,8 +3,9 @@
 //引数は、地点aとb(a,b=頂点の名前（0 or 1 or 2 or …）)とそのときの巡回路の通った順番を表すant.solution[CITY_NUM]
 //ant.solutiton[0]=0 で、かつ、ant.solution[CITY_NUM]（最後もう一度０）=0です
 
-void checkpath(int a, int b, int ant.solution[CITY_NUM]){ //配列を引数に渡すやり方が未解決です
+void checkpath(int city_a, int city_b, int*ant.solution){ //配列を引数に渡すやり方が未解決です
 	int i=0;
+	int ant.solution[CITY_NUM];
 	for(i=0;i<=CITY_NUM;i++){ //頂点a,bは何番目に訪れたのかを調べる。
 		if(ant.solution[i]==a){　//ant.solutionの配列の何番目にaを訪れたのか
 			order_a=i; //a は　i番目でした
